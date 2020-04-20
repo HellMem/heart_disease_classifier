e7fb77418f2bd632d5ad3381bb07b4fb7e75d371
HellMem <memo_coronado@hotmail.com>
2020-04-19 22:28:28 -0500

    Added different hyperparameters to test with

--- predictor.py
+++ predictor.py
@@ -23,30 +23,32 @@ def experiment(data, labels):
 
     parameter_space = {
         'hidden_layer_sizes': [(5, 2), (6, 2), (7, 2), (8, 2), (9, 2)],
-        'alpha': [0.001, 0.003, 0.01, 0.03, 0.1, 0.3]
+        'alpha': [0.001, 0.003, 0.01, 0.03, 0.1, 0.3],
+        'activation': ['tanh', 'relu'],
+        'learning_rate': ['constant', 'adaptive']
     }
 
     for hidden_layer_size in parameter_space['hidden_layer_sizes']:
         for alpha in parameter_space['alpha']:
-            nn_model = get_nn_model(X_train, y_train, hidden_layer_size, alpha)
+            for activation in parameter_space['activation']:
+                for learning_rate in parameter_space['learning_rate']:
+                    nn_model = get_nn_model(X_train, y_train, hidden_layer_size, alpha)
 
-            print('For {} hidden layers and {} learning rate:'.format(hidden_layer_size, alpha))
+                    print(
+                        'For {} hidden layers, {} activation, {} learning rate, and {} alpha:'.format(hidden_layer_size,
+                                                                                                      activation,
+                                                                                                      learning_rate,
+                                                                                                      alpha))
 
-            '''
-            pred_train_y = nn_model.predict(X_train)
-            print("Train report")
-            print(classification_report(y_train, pred_train_y))
+                    pred_test_y = nn_model.predict(X_test)
+                    print("Test f1-score")
+                    report = classification_report(y_test, pred_test_y, output_dict=True)
+                    test_results = pd.DataFrame(report).transpose()[:2]['f1-score']
+                    print('{} for negative classifications (no disease present)'.format(test_results[0]))
+                    print('{} for positive classifications (disease present)'.format(test_results[1]))
+                    print('-' * 100)
 
-            print('-' * 100)'''
-
-            pred_test_y = nn_model.predict(X_test)
-            print("Test report")
-            print(classification_report(y_test, pred_test_y))
-            print('-' * 100)
-            print('-' * 100)
-            print('-' * 100)
-
-        print('*' * 500)
+        print('*' * 100)
 
 
 if __name__ == "__main__":
